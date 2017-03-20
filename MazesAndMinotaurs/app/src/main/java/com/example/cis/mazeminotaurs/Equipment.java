31696f696e7a4380bf44498c3b5a6add21e68e20
Thorin Schmidt <tschmidt@stcharlessd.org>
2017-03-19 23:41:56 -0500

    Added most of the needed string resources for game elements. preliminary work on Equipment class.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/Equipment.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/Equipment.java
@@ -6,5 +6,57 @@ package com.example.cis.mazeminotaurs;
 
 public class Equipment {
     //instance variables
+    int mResId;
+    int mEncumberance;
+    double mCostInSp;
+    String mLongDescription;
 
+    public Equipment(int resId, int encumberance, double costInSp, String longDescription) {
+        mResId = resId;
+        mEncumberance = encumberance;
+        mCostInSp = costInSp;
+        mLongDescription = longDescription;
+    }
+
+    public int getResId() {
+        return mResId;
+    }
+
+    public void setResId(int resId) {
+        mResId = resId;
+    }
+
+    public int getEncumberance() {
+        return mEncumberance;
+    }
+
+    public void setEncumberance(int encumberance) {
+        mEncumberance = encumberance;
+    }
+
+    public double getCostInSp() {
+        return mCostInSp;
+    }
+
+    public void setCostInSp(double costInSp) {
+        mCostInSp = costInSp;
+    }
+
+    public String getLongDescription() {
+        return mLongDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        mLongDescription = longDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "mResId=" + mResId +
+                ", mEncumberance=" + mEncumberance +
+                ", mCostInSp=" + mCostInSp + "\n" +
+                ", mLongDescription='" + mLongDescription + '\'' +
+                '}';
+    }
 }
