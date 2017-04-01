f316bd4a45b149214a424497615e242a62c48561
Thorin Schmidt <tschmidt@stcharlessd.org>
2017-03-31 22:13:14 -0500

    Brand new baseline

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/MainMazes.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/MainMazes.java
@@ -1,76 +1,106 @@
 package com.example.cis.mazeminotaurs;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-public class MainMazes extends AppCompatActivity {
+public class MainMazes extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    AttributeScoreGenerator scoreGen = new AttributeScoreGenerator();
-    AttributeScore[] testAttributes = scoreGen.nextValidSet();
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
 
-    Button mNewChara;
-    Button mCharaCont;
-    Button mPlayerManual;
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.setDrawerListener(toggle);
+            toggle.syncState();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_mazes);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
 
-        for(int i = 0; i < testAttributes.length; i++){
-            System.out.println(testAttributes[i].getScore());
+            Fragment contentFragment = new SplashScreenFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, contentFragment);
+            ft.commit();
         }
 
-        mNewChara = (Button) findViewById(R.id.new_character_bttn);
-        mNewChara.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                setContentView(R.layout.fragment_character_creator);
+        @Override
+        public void onBackPressed() {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
             }
-        });
+        }
 
-        mCharaCont = (Button) findViewById(R.id.character_continue_bttn);
-        mCharaCont.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setContentView(R.layout.activity_character_sheet);
-            }
-        });
-        mPlayerManual = (Button) findViewById(R.id.player_manual_bttn);
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main_mazes, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        Fragment contentFrag = null;
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_help) {
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.main, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
+
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+            Fragment contentFragment = new SplashScreenFragment();
+
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, contentFragment);
+            ft.commit();
+
+            if (id == R.id.create_character) {
+                // Handle the camera action
+            } else if (id == R.id.play_character) {
+
+            } else if (id == R.id.delete_character) {
+
+            } else if (id == R.id.player_manual) {
+
+            } else if (id == R.id.player_comapanion) {
+
+            } else if (id == R.id.website) {
+
+            }
+
+            if (contentFragment != null){
+                ft.replace(R.id.content_frame, contentFragment);
+                ft.commit();
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
 }
---
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/SplashScreenFragment.java
@@ -0,0 +1,21 @@
+package com.example.cis.mazeminotaurs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Thorin Schmidt on 3/31/2017.
+ */
+
+public class SplashScreenFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater li, ViewGroup vg, Bundle b){
+        super.onCreateView(li, vg, b);
+        View rootView = li.inflate(R.layout.fragment_splash_screen, vg, false);
+
+        return rootView;
+    }
+}
