e70eafb1afcb002e64d7fe1998555c5ef593571a
Noel Kling <ckling@PC19.cis.com>
2017-04-05 10:13:14 -0500

    Worked on a few pieces of CharacterCreation

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/NewCharacter/CharacterCreationFragment.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/NewCharacter/CharacterCreationFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.cis.mazeminotaurs.R;
 
@@ -13,10 +14,19 @@ import com.example.cis.mazeminotaurs.R;
  */
 
 public class CharacterCreationFragment extends Fragment {
+
+    public ImageButton mWarriorButton;
+    public ImageButton mMagicianButton;
+    public ImageButton mSpecialistButton;
+
     @Override
     public View onCreateView(LayoutInflater li, ViewGroup vg, Bundle b){
         super.onCreateView(li, vg, b);
         View rootView = li.inflate(R.layout.fragment_character_creation, vg, false);
+
+        mWarriorButton = (ImageButton) rootView.findViewById(R.id.warrior_button);
+        mMagicianButton = (ImageButton) rootView.findViewById(R.id.magician_button);
+        mSpecialistButton = (ImageButton) rootView.findViewById(R.id.specialist_button);
 
         return rootView;
     }
