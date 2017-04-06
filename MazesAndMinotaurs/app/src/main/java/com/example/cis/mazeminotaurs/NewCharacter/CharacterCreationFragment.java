cf1dd0c75c1f9afc046c8eea119368826805d9e5
Noel Kling <ckling@PC19.cis.com>
2017-04-06 10:07:33 -0500

    Added variables to Fragment

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/NewCharacter/CharacterCreationFragment.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/NewCharacter/CharacterCreationFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.example.cis.mazeminotaurs.R;
 
@@ -19,6 +21,16 @@ public class CharacterCreationFragment extends Fragment {
     public ImageButton mMagicianButton;
     public ImageButton mSpecialistButton;
 
+    public RadioGroup mWarriorGroup;
+    public RadioGroup mMagicianGroup;
+    public RadioGroup mSpecialistGroup;
+
+    public RadioButton mAmazonRadio;
+    public RadioButton mBarbarianRadio;
+    public RadioButton mCentaurRadio;
+    public RadioButton mNobleRadio;
+    public RadioButton mSpearRadio;
+
     @Override
     public View onCreateView(LayoutInflater li, ViewGroup vg, Bundle b){
         super.onCreateView(li, vg, b);
@@ -27,6 +39,11 @@ public class CharacterCreationFragment extends Fragment {
         mWarriorButton = (ImageButton) rootView.findViewById(R.id.warrior_button);
         mMagicianButton = (ImageButton) rootView.findViewById(R.id.magician_button);
         mSpecialistButton = (ImageButton) rootView.findViewById(R.id.specialist_button);
+
+        mWarriorGroup = (RadioGroup) rootView.findViewById(R.id.warrior_radio_set);
+        mMagicianGroup = (RadioGroup) rootView.findViewById(R.id.magician_radio_set);
+        mSpecialistGroup = (RadioGroup) rootView.findViewById(R.id.specialist_radio_set);
+
 
         return rootView;
     }
