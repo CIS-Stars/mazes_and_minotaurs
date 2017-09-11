55dacee676f5b4b24d37fef233b439dee6a2f7d1
jhiggs <jhiggs@stcharlessd.org>
2017-09-11 13:45:03 -0500

    Updated gradle.

---
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/classes/Priest.java
@@ -0,0 +1,28 @@
+package com.example.cis.mazeminotaurs.character.classes;
+
+import com.example.cis.mazeminotaurs.AttributeScore;
+import com.example.cis.mazeminotaurs.Equipment;
+import com.example.cis.mazeminotaurs.EquipmentDB;
+import com.example.cis.mazeminotaurs.R;
+import com.example.cis.mazeminotaurs.Weapon;
+import com.example.cis.mazeminotaurs.character.PlayerCharacter;
+import com.example.cis.mazeminotaurs.character.Gender;
+import com.example.cis.mazeminotaurs.character.stats.Score;
+import com.example.cis.mazeminotaurs.util.Util;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
+
+/**
+ * Created by jhiggs on 9/11/17.
+ */
+
+public class Priest extends Magician{
+    @Override
+    public int getMysticalStrength() {
+        return super.getMysticalStrength();
+    }
+}
