5975bb14d05d39c17c199ef455914b4d543140c1
jhiggs <jhiggs@stcharlessd.org>
2017-09-12 13:30:45 -0500

    Updated gradle.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/classes/Priest.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/classes/Priest.java
@@ -20,9 +20,122 @@ import java.util.HashMap;
  * Created by jhiggs on 9/11/17.
  */
 
-public class Priest extends Magician{
-    @Override
-    public int getMysticalStrength() {
-        return super.getMysticalStrength();
+public class Priest extends Magician implements Level{
+    private ArrayList<HashMap<Score, Integer>> mScoreLevelChoice = new ArrayList<>();
+
+    public Priest(PlayerCharacter playerCharacter) {
+        Score[] primAttrs = {Score.SKILL, Score.WILL};
+        ArrayList<Score> primAttributes = new ArrayList<>();
+        Collections.addAll(primAttributes, primAttrs);
+
+        EquipmentDB equipmentDB = EquipmentDB.getInstance();
+        ArrayList<Equipment> startGear = new ArrayList<>();
+
+        int rolledGold = Util.roll(6, 3) * 10;
+
+        startGear.add(equipmentDB.getWeapon(R.string.staff));
+        startGear.add(equipmentDB.getWeapon(R.string.dagger));
+        //TODO add robes
+
+        setBasicHits(8);
+        setCharacter(playerCharacter);
+        setPrimaryAttributes(primAttributes);
+        setRequiredGender(Gender.EITHER);
+        setResId(Classes.PRIEST.getResId());
+        setStartGold(rolledGold);
+        setStartGear(startGear);
+
+    }
+
+    public void doLevelUp(){
+        Score[] possibleScores = {Score.SKILL, Score.WILL, Score.MIGHT, Score.WITS};
+        doLevelUp(possibleScores[Util.roll(possibleScores.length) - 1]);
+    }
+
+    public void doLevelUp(Score score) {
+        if (getLevel() < getEffectiveLevel()){
+
+            Score[] choices = {Score.SKILL, Score.WILL, Score.MIGHT, Score.WITS};
+            ArrayList<Score> possibleScores = new ArrayList<>();
+            for (Score selectScore: choices) {
+                if(getCharacter().canAddToScore(selectScore)) {
+                    possibleScores.add(selectScore);
+                }
+            }
+
+            Score selectedScore;
+            if (possibleScores.contains(score)) {
+                selectedScore = score;
+            } else {
+                selectedScore = possibleScores.get(Util.roll(possibleScores.size()) - 1);
+            }
+
+            if (possibleScores.size() > 0) {
+                while (!getCharacter().canAddToScore(selectedScore)) {
+                    selectedScore = possibleScores.get((possibleScores.indexOf(selectedScore) + 1) % possibleScores.size());
+                }
+            }
+
+            // Contains information about changed scores
+            HashMap<Score, Integer> levelData = new HashMap<>();
+
+            if (getCharacter().canAddToScore(Score.LUCK)) {
+                AttributeScore luck = getCharacter().getScore(Score.LUCK);
+                levelData.put(Score.LUCK, luck.getScore());
+                luck.setScore(luck.getScore() + 1);
+            } else {
+                levelData.put(Score.LUCK, 20);
+            }
+
+            AttributeScore selectedAttrScore = getCharacter().getScore(selectedScore);
+
+            levelData.put(selectedScore, selectedAttrScore.getScore());
+            getScoreLevelChoice().add(levelData);
+
+            selectedAttrScore.setScore(selectedAttrScore.getScore() + 2);
+            setAddedHits(getAddedHits() + 2);
+
+            setLevel(getLevel() + 1);
+            getCharacter().validateScores();
+        }
+    }
+
+    public void doLevelDown(){
+        if (getLevel() > 1) {
+            HashMap<Score, Integer> levelData = getScoreLevelChoice().remove(getScoreLevelChoice().size() - 1);
+            Score lastSelectedScore = null;
+            for (Object rawScore: levelData.keySet().toArray()) {
+                Score score = (Score) rawScore;
+                if (score != Score.LUCK) {
+                    lastSelectedScore = score;
+                    break;
+                }
+            }
+
+            AttributeScore luck = getCharacter().getScore(Score.LUCK);
+            AttributeScore lastScoreLeveled = getCharacter().getScore(lastSelectedScore);
+
+            setAddedHits(getAddedHits() - 2);
+            luck.setScore(levelData.get(Score.LUCK));
+            lastScoreLeveled.setScore(levelData.get(lastSelectedScore));
+            setLevel(getLevel() - 1);
+        }
+    }
+
+    public ArrayList<HashMap<Score, Integer>> getScoreLevelChoice() {
+        return mScoreLevelChoice;
+    }
+
+    public void setScoreLevelChoice(ArrayList<HashMap<Score, Integer>> scoreLevelChoice) {
+        mScoreLevelChoice = scoreLevelChoice;
+    }
+
+    public int getDefensiveFightingBonus() {
+        return getCharacter().getScore(Score.SKILL).getModifier();
+    }
+
+    public int getMartialDisciplineBonus() {
+        return getCharacter().getScore(Score.WILL).getModifier();
     }
 }
+
