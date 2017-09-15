d9baac99aa2f59b6529b3badbce6461dae96fbcc
JayTSmith <justinsmth42@gmail.com>
2017-09-15 12:02:53 -0500

    Created DetailFragment class

---
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/NewCharacter/dialogs/DetailFragment.java
@@ -0,0 +1,10 @@
+package com.example.cis.mazeminotaurs.NewCharacter.dialogs;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by jsmith on 9/15/17.
+ */
+
+public class DetailFragment extends DialogFragment{
+}
