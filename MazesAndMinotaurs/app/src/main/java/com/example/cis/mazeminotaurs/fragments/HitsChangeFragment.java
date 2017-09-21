159e2e1c2a2c9f4358bfa70354c0fa77a078f0b2
JayTSmith <justinsmth42@gmail.com>
2017-09-21 11:43:15 -0500

    Created the HitsChangeFragment class

---
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/fragments/HitsChangeFragment.java
@@ -0,0 +1,10 @@
+package com.example.cis.mazeminotaurs.fragments;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by jsmith on 9/21/17.
+ */
+
+public class HitsChangeFragment extends DialogFragment {
+}
