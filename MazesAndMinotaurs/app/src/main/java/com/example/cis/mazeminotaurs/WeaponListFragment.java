94359863ae815bf966466d5538c8915453af5e3e
JayTSmith <justinsmth42@gmail.com>
2017-09-21 12:50:41 -0500

    MID-WORK COMMIT - This is to simply save work.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/WeaponListFragment.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/WeaponListFragment.java
@@ -1,14 +1,62 @@
 package com.example.cis.mazeminotaurs;
 
+import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by Thorin Schmidt on 5/4/2017.
  */
 
 public class WeaponListFragment extends DialogFragment {
-
+    public interface onWeaponChangeListener {
+        void onWeaponChange(Weapon newWeapon);
+    }
+    onWeaponChangeListener mWeaponChangeListener;
+    
     private RecyclerView mWeaponRecyclerView;
+    
+    /**
+     * Create a new instance of WeaponListFragment, providing "weaponId"
+     * as an arguments.
+     */
+    public static WeaponListFragment newInstance(int weaponId) {
+        WeaponListFragment f = new WeaponListFragment();
 
+        // Supply num input as an argument.
+        Bundle args = new Bundle();
+        args.putInt("weaponId", weaponId);
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater li, ViewGroup vg, Bundle savedInstanceState) {
+        super.onCreateView(li, vg, savedInstanceState);
+        View v = li.inflate(R.layout.dialog_weapon_list, vg, false);
+
+        mWeaponRecyclerView = (RecyclerView) v.findViewById(R.id.weapon_recyler_view);
+        ArrayAdapter<Weapon> adapter = new ArrayAdapter<Weapon>(EquipmentDB.getInstance().);
+
+        mWeaponRecyclerView.setAdapter();
+
+        return v;
+    }
+
+
+    public onWeaponChangeListener getWeaponChangeListener() {
+        return mWeaponChangeListener;
+    }
+
+    public void setWeaponChangeListener(onWeaponChangeListener weaponChangeListener) {
+        mWeaponChangeListener = weaponChangeListener;
+    }
 }
