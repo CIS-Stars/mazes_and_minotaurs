c36a5b69964657328562d023dc1cb29e75a93cb4
JayTSmith <justinsmth42@gmail.com>
2017-10-16 11:32:21 -0500

    Simplified SaveAndLoadPerformer

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/SaveAndLoadPerformer.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/SaveAndLoadPerformer.java
@@ -40,9 +40,7 @@ public class SaveAndLoadPerformer {
      * @return json string of playerCharacter
      */
     public static String save(int characterIndex) {
-        GsonBuilder gson = new GsonBuilder();
-        gson.registerTypeAdapter(PlayerCharacter.class, new CharacterSerializer());
-        return gson.create().toJson(Portfolio.get().getPlayerCharacter(characterIndex));
+        return new Gson().toJson(Portfolio.get().getPlayerCharacter(characterIndex));
     }
 
     /**
@@ -51,56 +49,6 @@ public class SaveAndLoadPerformer {
      * @return the generated playerCharacter
      */
     public static PlayerCharacter load(String jsonString){
-        PlayerCharacter returnCharacter = new PlayerCharacter();
-        JsonObject loadedData = (JsonObject) new Gson().fromJson(jsonString, JsonElement.class);
-
-        for (Score score: Score.values()) {
-            int loadedScore = loadedData.getAsJsonObject("mScores").getAsJsonObject(score.toString()).get("mScore").getAsInt();
-            returnCharacter.getScore(score).setScore(loadedScore);
-        }
-        Gson gson = new Gson();
-
-        returnCharacter.setAge(gson.fromJson(loadedData.get("mAge"), Integer.class));
-        returnCharacter.setBreastplate(gson.fromJson(loadedData.get("mBreastplate"), Armor.class));
-        returnCharacter.setCharClass(gson.fromJson(loadedData.get("mCharClass"), Barbarian.class));
-        returnCharacter.setCurrentWeapon(gson.fromJson(loadedData.get("mCurrentWeapon"), Weapon.class));
-        returnCharacter.setGender(gson.fromJson(loadedData.get("mGender"), Gender.class));
-        returnCharacter.setHelmet(gson.fromJson(loadedData.get("mHelmet"), Armor.class));
-        returnCharacter.setMoney(gson.fromJson(loadedData.get("mMoney"), HashMap.class));
-        returnCharacter.setName(loadedData.get("mName").getAsString());
-        returnCharacter.setInventory(gson.fromJson(loadedData.get("mInventory"), ArrayList.class));
-        returnCharacter.setShield(gson.fromJson(loadedData.get("mShield"), Armor.class));
-
-        return returnCharacter;
-    }
-}
-
-/**
- * Custom serializer for transforming the PlayerCharacter class into json.
- */
-class CharacterSerializer implements JsonSerializer<PlayerCharacter> {
-    @Override
-    public JsonElement serialize(PlayerCharacter src, Type typeOfSrc, JsonSerializationContext context) {
-        BaseClass characterClass = src.getCharClass();
-        characterClass.setCharacter(null);
-
-        JsonObject rootObject = new JsonObject();
-        rootObject.add("mScores", context.serialize(src.getScores()));
-        rootObject.add("mCharClass", context.serialize(src.getCharClass()));
-        rootObject.add("mGender", context.serialize(src.getGender()));
-        rootObject.add("mMoney", context.serialize(src.getMoney()));
-        rootObject.add("mAge", context.serialize(src.getAge()));
-        rootObject.add("mName",context.serialize(src.getName()));
-        rootObject.add("mInventory", context.serialize(src.getInventory()));
-        rootObject.add("mCurrentWeapon", context.serialize(src.getCurrentWeapon()));
-        rootObject.add("mHelmet", context.serialize(src.getHelmet()));
-        rootObject.add("mBreastplate", context.serialize(src.getBreastplate()));
-        rootObject.add("mShield",context.serialize(src.getShield()));
-
-        JsonElement classJson = context.serialize(characterClass);
-        rootObject.add("mCharClass", classJson);
-
-        characterClass.setCharacter(src);
-        return rootObject;
+        return new Gson().fromJson(jsonString, PlayerCharacter.class);
     }
 }
