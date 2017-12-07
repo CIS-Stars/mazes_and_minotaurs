10f15bfa08a78c020e3c44b7a9a21fbc3ab06a61
JayTSmith <justinsmth42@gmail.com>
2017-12-07 13:08:16 -0600

    Added documentation to Spearman.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/classes/Spearman.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/classes/Spearman.java
@@ -13,14 +13,25 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * Created by jusmith on 5/15/17.
+ * This class represents the Spearman (Normal) that is in the game.
+ *
+ * @author jusmith on 5/15/17.
  */
 
 public class Spearman extends Warrior {
+    /**
+     * Blank constructor. Used primarily for reflection purposes.
+     * <b>DO NOT USE THIS FOR UI DISPLAYS.</b>
+     */
     public Spearman() {
         this(null);
     }
 
+    /**
+     * Constructor that requires a {@link PlayerCharacter} instance.
+     *
+     * @param playerCharacter the character using this class
+     */
     public Spearman(PlayerCharacter playerCharacter) {
         setPossibleStartWeapons(new Weapon[]{});
         setPossibleWeaponsOfChoice(new Weapon[]{EquipmentDB.getInstance().getWeapon(CommonStrings.SPEAR.getValue())});
@@ -55,10 +66,25 @@ public class Spearman extends Warrior {
         setWeaponOfChoice(equipmentDB.getWeapon(CommonStrings.SPEAR.getValue()));
     }
 
+    /**
+     * The addition made to the EDC of the character, unless surprised.
+     * Must be fighting with spear and shield; also, does not apply
+     * to missile attacks.
+     * <i>As of 12/7/17, this is not used in the code.</i>
+     *
+     * @return Character's modifier of their Skill score.
+     */
     public int getDefensiveFightingBonus() {
         return getCharacter().getScore(Score.SKILL).getModifier();
     }
 
+    /**
+     * The addition made to the Initiative of the character when fighting
+     * with a spear.
+     * <i>As of 12/7/17, this is not used in the code.</i>
+     *
+     * @return Character's modifier of their Will score.
+     */
     public int getMartialDisciplineBonus() {
         return getCharacter().getScore(Score.WILL).getModifier();
     }
