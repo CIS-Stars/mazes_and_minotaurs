30f8fd25d3e71c57a1d48be5bb52a749b74c5f06
JayTSmith <justinsmth42@gmail.com>
2017-12-08 12:52:32 -0600

    Added documentation to CommonStrings.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/util/CommonStrings.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/util/CommonStrings.java
@@ -1,7 +1,9 @@
 package com.example.cis.mazeminotaurs.util;
 
 /**
- * Created by JayTSmith on 10/25/17.
+ * An Enumeration of default equipment values.
+ * Also, contains the arguments used for keys in bundle arguments.
+ * @author jsmith on 10/25/17.
  */
 
 public enum CommonStrings {
@@ -51,12 +53,24 @@ public enum CommonStrings {
     CHARACTER_ARG("CHARACTER_ARG"),
     SCORE_ARG("SCORE_ARG");
 
+    /**
+     * The string value of the enum's value name.
+     */
     String value;
 
+    /**
+     * Default constructor.
+     *
+     * @param value Arbitrary value.
+     */
     CommonStrings(String value) {
         this.value = value;
     }
 
+    /**
+     * The string value of the enum.
+     * @return a string.
+     */
     public String getValue() {
         return value;
     }
