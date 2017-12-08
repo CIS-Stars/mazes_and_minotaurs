ec68e096d63e7792a264d1ae025ae1da57af4dfd
JayTSmith <justinsmth42@gmail.com>
2017-12-08 12:56:18 -0600

    Added a package-info in rollDice package, and updated others' author tags.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/classes/package-info.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/character/classes/package-info.java
@@ -1,8 +1,8 @@
 /**
  * This is used to setup up the game classes for use in the application.
  *
+ * @author James Higgs (jhiggs)
  * @author Justin Smith (jsmith)
  * @author Zack Steck (zsteck)
- * @author James Higgs (jhiggs)
  */
 package com.example.cis.mazeminotaurs.character.classes;
