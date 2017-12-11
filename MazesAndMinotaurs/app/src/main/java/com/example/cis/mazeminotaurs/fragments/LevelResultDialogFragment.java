4da4393da4ea58a1a689d3f7614f8ec8e01b61d6
JayTSmith <justinsmth42@gmail.com>
2017-12-11 11:26:58 -0600

    Added documentation for LevelResultDialogFragment. Also added details.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/fragments/LevelResultDialogFragment.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/fragments/LevelResultDialogFragment.java
@@ -20,17 +20,39 @@ import com.example.cis.mazeminotaurs.util.CommonStrings;
 import java.util.Locale;
 
 /**
- * Created by JayTSmith on 12/6/17.
+ * This fragment displays what the results of the character's level up would be.
+ * The user can confirm the level up here.
+ *
+ * @see ExperienceDialogFragment
+ * @author jsmith on 12/6/17.
  */
 
 public class LevelResultDialogFragment extends DialogFragment {
+    /**
+     * Serves as the TAG in certain functions. e.g. dialog.show() and logging.
+     */
     public static final String TAG = LevelStatSelectDialogFragment.class.getName();
 
+    /**
+     * The root dialog instance.
+     *
+     * @see ExperienceDialogFragment
+     */
     private ExperienceDialogFragment mRootDialog;
 
+    /**
+     * This is the class of the character that is being leveled up.
+     */
     private BaseClass mCharClass;
+
+    /**
+     * The score selected to improve on level up.
+     */
     private Score mSelectedScore;
 
+    /*
+     * These are the widgets found in the layout.
+     */
     private TextView mLuckAddTextView;
     private TextView mHitAddTextView;
     private TextView mScoreHeadTextView;
@@ -73,6 +95,13 @@ public class LevelResultDialogFragment extends DialogFragment {
                 .create();
     }
 
+    /**
+     * This is a helper method for created the onclick listener for the positive
+     * button. The listener will do the level up and calls the root dialog's
+     * listener.
+     *
+     * @return the onclick listener for the positive button
+     */
     private DialogInterface.OnClickListener getOnPositiveListener() {
         return new DialogInterface.OnClickListener() {
             @Override
@@ -83,10 +112,19 @@ public class LevelResultDialogFragment extends DialogFragment {
         };
     }
 
+    /**
+     * Getter for the mRootDialog property.
+     *
+     * @return the value of mRootDialog.
+     */
     public ExperienceDialogFragment getRootDialog() {
         return mRootDialog;
     }
 
+    /**
+     * Setter for the mRootDialog property.
+     * @param rootDialog the new value of mRootDialog.
+     */
     public void setRootDialog(ExperienceDialogFragment rootDialog) {
         mRootDialog = rootDialog;
     }
