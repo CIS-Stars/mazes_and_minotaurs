0c9e0c4a4f41a09dd973a3a85fe791b7208611ec
JayTSmith <justinsmth42@gmail.com>
2017-12-12 13:03:23 -0600

    Added docs to the rest of the classes.

--- MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/MainMazes.java
+++ MazesAndMinotaurs/app/src/main/java/com/example/cis/mazeminotaurs/MainMazes.java
@@ -31,149 +31,153 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
+/**
+ * The main activity of Mazes and Minotaurs.
+ * This is the one that opens on startup.
+ */
 public class MainMazes extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            if (!Portfolio.get().isLoaded()) {
-                loadPortfolio();
-                Portfolio.get().setLoaded(true);
-            }
-            if (!EquipmentDB.getInstance().isLoaded()) {
-                loadEDB();
-                EquipmentDB.getInstance().setLoaded(true);
-            }
-
-            setContentView(R.layout.activity_main);
-            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-            setSupportActionBar(toolbar);
-
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-            drawer.setDrawerListener(toggle);
-            toggle.syncState();
-
-            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-            navigationView.setNavigationItemSelectedListener(this);
-
-            Fragment contentFragment = new SplashScreenFragment();
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.content_frame, contentFragment);
-            ft.commit();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!Portfolio.get().isLoaded()) {
+            loadPortfolio();
+            Portfolio.get().setLoaded(true);
+        }
+        if (!EquipmentDB.getInstance().isLoaded()) {
+            loadEDB();
+            EquipmentDB.getInstance().setLoaded(true);
         }
 
-        @Override
-        public void onBackPressed() {
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-            if (drawer.isDrawerOpen(GravityCompat.START)) {
-                drawer.closeDrawer(GravityCompat.START);
-            } else {
-                super.onBackPressed();
-            }
-        }
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.main, menu);
-            return true;
-        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
-            } else if (id == R.id.action_save_portfolio) {
-                try {
-                    FileOutputStream fos = getApplicationContext().openFileOutput(Portfolio.FILENAME, Context.MODE_PRIVATE);
-                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fos);
-                    outputStreamWriter.write(SaveAndLoadPerformer.savePortfolio());
-                    outputStreamWriter.close();
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return true;
-            } else if (id == R.id.action_save_equipment) {
-                try {
-                    FileOutputStream fos = getApplicationContext().openFileOutput(EquipmentDB.FILENAME, Context.MODE_PRIVATE);
-                    OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fos);
-                    outputStreamWriter.write(SaveAndLoadPerformer.saveEquipmentDB());
-                    outputStreamWriter.close();
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return true;
-            } else if (id == R.id.action_reset_equipment) {
-                AlertDialog dialog = new AlertDialog.Builder(this, R.style.Theme_AppCompat)
-                        .setTitle(R.string.caution_alert)
-                        .setMessage(R.string.reset_equip_msg)
-                        .setPositiveButton(R.string.confirm_button, new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                getApplicationContext().deleteFile(EquipmentDB.FILENAME);
-                                EquipmentDB.getInstance().resetDatabase();
-                            }
-                        })
-                        .setNegativeButton(R.string.cancel, null).create();
-                dialog.show();
-            }
+        Fragment contentFragment = new SplashScreenFragment();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, contentFragment);
+        ft.commit();
+    }
 
-            return super.onOptionsItemSelected(item);
-        }
-
-        @SuppressWarnings("StatementWithEmptyBody")
-        @Override
-        public boolean onNavigationItemSelected(MenuItem item) {
-            // Handle navigation view item clicks here.
-            int id = item.getItemId();
-            Intent actIntent = null;
-            Fragment contentFragment = new SplashScreenFragment();
-
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.content_frame, contentFragment);
-            ft.commit();
-            
-            // Clears the back stack
-            Util.clearBackStack(this);
-
-            if (id == R.id.create_character) {
-               contentFragment = new CharacterCreationFragment();
-            } else if (id == R.id.play_character) {
-                actIntent = new Intent(this, CharacterPlayActivity.class);
-            } else if (id == R.id.manage_characters) {
-                actIntent = new Intent(this, CharacterManageActivity.class);
-            } else if (id == R.id.player_manual) {
-                contentFragment = new PlayerManualFragment();
-            } else if (id == R.id.player_comapanion) {
-                contentFragment = new CompanionFragment();
-            } else if (id == R.id.website) {
-                contentFragment = new WebsiteFragment();
-            }
-
-            if (actIntent != null) {
-                startActivity(actIntent);
-            } else if (contentFragment != null) {
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.content_frame, contentFragment)
-                        .commit();
-            }
-
-            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-            return true;
+        } else {
+            super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        } else if (id == R.id.action_save_portfolio) {
+            try {
+                FileOutputStream fos = getApplicationContext().openFileOutput(Portfolio.FILENAME, Context.MODE_PRIVATE);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fos);
+                outputStreamWriter.write(SaveAndLoadPerformer.savePortfolio());
+                outputStreamWriter.close();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        } else if (id == R.id.action_save_equipment) {
+            try {
+                FileOutputStream fos = getApplicationContext().openFileOutput(EquipmentDB.FILENAME, Context.MODE_PRIVATE);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fos);
+                outputStreamWriter.write(SaveAndLoadPerformer.saveEquipmentDB());
+                outputStreamWriter.close();
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
+        } else if (id == R.id.action_reset_equipment) {
+            AlertDialog dialog = new AlertDialog.Builder(this, R.style.Theme_AppCompat)
+                    .setTitle(R.string.caution_alert)
+                    .setMessage(R.string.reset_equip_msg)
+                    .setPositiveButton(R.string.confirm_button, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            getApplicationContext().deleteFile(EquipmentDB.FILENAME);
+                            EquipmentDB.getInstance().resetDatabase();
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel, null).create();
+            dialog.show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        Intent actIntent = null;
+        Fragment contentFragment = new SplashScreenFragment();
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content_frame, contentFragment);
+        ft.commit();
+
+        // Clears the back stack
+        Util.clearBackStack(this);
+
+        if (id == R.id.create_character) {
+            contentFragment = new CharacterCreationFragment();
+        } else if (id == R.id.play_character) {
+            actIntent = new Intent(this, CharacterPlayActivity.class);
+        } else if (id == R.id.manage_characters) {
+            actIntent = new Intent(this, CharacterManageActivity.class);
+        } else if (id == R.id.player_manual) {
+            contentFragment = new PlayerManualFragment();
+        } else if (id == R.id.player_comapanion) {
+            contentFragment = new CompanionFragment();
+        } else if (id == R.id.website) {
+            contentFragment = new WebsiteFragment();
+        }
+
+        if (actIntent != null) {
+            startActivity(actIntent);
+        } else if (contentFragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.content_frame, contentFragment)
+                    .commit();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     /**
      * Helper method for loading the portfolio on startup.
@@ -200,6 +204,9 @@ public class MainMazes extends AppCompatActivity
         }
     }
 
+    /**
+     * Helper method for loading the equipment database on startup.
+     */
     private void loadEDB() {
         try {
             String basePath = getApplicationContext().getFilesDir().getPath() + "/";
